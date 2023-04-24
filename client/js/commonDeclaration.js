7ffef2b7afaa4f4f7da6f369af534fea8905ed89
Raghevndra Gautam <raghvendragautam240@gmail.com>
2023-04-24 20:20:38 +0530

    Changed URLs

--- client/js/commonDeclaration.js
+++ client/js/commonDeclaration.js
@@ -1,7 +1,7 @@
 const BASE_FE_URL = 'https://daanapis.onrender.com';
-const BASE_BE_URL = 'https://daanapis.onrender.com/';
-const BASE_SOCKET_URL = 'https://daanapis.onrender.com/';
-const SOCKET_URL = 'https://daanapis.onrender.com/';
+const BASE_BE_URL = 'https://daanapis.onrender.com';
+const BASE_SOCKET_URL = 'https://daanapis.onrender.com';
+const SOCKET_URL = 'https://daanapis.onrender.com';
 // const BASE_FE_URL = 'http://localhost:5500';
 // const BASE_BE_URL = 'http://localhost:3000';
 // const BASE_SOCKET_URL = 'http://localhost:3000';
